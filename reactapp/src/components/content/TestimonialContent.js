fb02f86c5ac5694eb3070558b047e3dee35ac10f
aravindd7 <ak154761@gmail.com>
2022-01-24 16:39:58 +0530

    favicon added

--- reactapp/src/components/content/TestimonialContent.js
+++ reactapp/src/components/content/TestimonialContent.js
@@ -6,23 +6,37 @@ import { Carousel } from "react-bootstrap";
 export default function TestimonialContent() {
     return (
         <div className="content-testimonial">
-         <Carousel>
-            <div className="container-row">
-            <BsFillArrowLeftCircleFill />
-            <div>
-            <div>
+         <Carousel variant="dark">
+            <Carousel.Item>
             <img  className="img-box-inner" src="../images/client.jpg"/>
-            </div>
             <h5>Anna Trevor</h5>
             <h6>Customer</h6>
-            </div>
-            <BsFillArrowRightCircleFill />
-            </div>
             <p>Dignissimos reprehenderit repellendus nobis error quibusdam?
              Atque animi sint unde quis reprehenderit, et,<br />perspiciatis, 
              debitis totam est deserunt eius officiis ipsum ducimus ad labore 
              modi voluptatibus accusantium <br /> sapiente nam! Quaerat.
              </p>
+             </Carousel.Item>
+             <Carousel.Item>
+            <img  className="img-box-inner" src="../images/client.jpg"/>
+            <h5>Anna Trevor</h5>
+            <h6>Customer</h6>
+            <p>Dignissimos reprehenderit repellendus nobis error quibusdam?
+             Atque animi sint unde quis reprehenderit, et,<br />perspiciatis, 
+             debitis totam est deserunt eius officiis ipsum ducimus ad labore 
+             modi voluptatibus accusantium <br /> sapiente nam! Quaerat.
+             </p>
+             </Carousel.Item>
+             <Carousel.Item>
+            <img  className="img-box-inner" src="../images/client.jpg"/>
+            <h5>Anna Trevor</h5>
+            <h6>Customer</h6>
+            <p>Dignissimos reprehenderit repellendus nobis error quibusdam?
+             Atque animi sint unde quis reprehenderit, et,<br />perspiciatis, 
+             debitis totam est deserunt eius officiis ipsum ducimus ad labore 
+             modi voluptatibus accusantium <br /> sapiente nam! Quaerat.
+             </p>
+             </Carousel.Item>
          </Carousel>
         </div>
     );
