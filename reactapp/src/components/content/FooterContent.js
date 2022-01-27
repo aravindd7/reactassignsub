eadbe4ec4dc55b6e40488418caaeea2740706905
aravindd7 <ak154761@gmail.com>
2022-01-27 15:24:49 +0530

    responsive progress

--- reactapp/src/components/content/FooterContent.js
+++ reactapp/src/components/content/FooterContent.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import { BsCollection } from 'react-icons/bs';
 export default function FooterContent() {
     return(
         <div>
-            <div className='footer-home'>
-            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
+            <Container fluid>
+            <Row className='footer-home'>
+            <Col style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
                 <img src='../images/logo.png' width="300px" style={{marginBottom: '10px'}}/>
                 <p><span style={{fontWeight: 'bold'}}>ADDRESS:</span> 28 White tower, Street Name New York<br />City, USA</p>
                 <p><span style={{fontWeight: 'bold'}}>TELEPHONE:</span> +91 9876543210</p>
                 <p><span style={{fontWeight: 'bold'}}>EMAIL:</span>yourmain@gmail.com</p>
-            </div>
-            <div>
+            </Col>
+            <Col>
             <h3>Menu</h3>
             <ul className='ul-footer'>
                 <li>Home</li>
@@ -20,8 +23,8 @@ export default function FooterContent() {
                 <li>Blog</li>
                 <li>Contact</li>
             </ul>
-            </div>
-            <div>
+            </Col>
+            <Col>
             <h3>Account</h3>
             <ul className='ul-footer'>
                 <li>Account</li>
@@ -31,16 +34,17 @@ export default function FooterContent() {
                 <li>Shopping</li>
                 <li>Widget</li>
             </ul>
-            </div>
-            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
+            </Col>
+            <Col style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
                 <h3>Newsletter</h3>
                 <p>Subscribe by our newsletter and get update protidin.</p>
                 <div className='container-row'>
                     <input name="email" placeholder='Enter Your Mail' />
                     <Button className='btn-red'>Subscribe</Button>
                 </div>
-            </div>
-            </div>
+            </Col>
+            </Row>
+            </Container>
             <div className='copyright'><p>©2021 All Rights Reserved By Free Html Templates</p></div>     
         </div>
     );
--- reactapp/src/components/content/FooterOther.js
+++ reactapp/src/components/content/FooterOther.js
@@ -8,34 +8,43 @@ import { BsPinterest } from "react-icons/bs";
 import { IoCall } from "react-icons/io5";
 import { AiOutlineMail } from "react-icons/ai";
 import { Link } from 'react-router-dom';
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function FooterOther() {
     return (
         <div className="footer-other">
-            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start',marginLeft: '20px'}}>
-                <h4>Reach at...</h4>
-                <a className="hover-red"><GoLocation /><span>Location</span></a>
-                <a className="hover-red"><IoCall /><span>+01-1234567890</span></a>
-                <a className="hover-red"><AiOutlineMail /><span>demo@gmail.com</span></a>
-            </div>
-            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginLeft: '200px'}}>
-                <Link to="/" className="link"><h4>Famms</h4></Link> 
-                <p>Necessary, making this the
-                 first true<br />generator on the Internet.
-                  It uses a<br />dictionary of over 200 Latin words, <br /> 
-                  combined with</p>
-                <div className="container-row">
-                    <BsFacebook style={{marginLeft: '10px', height: '25px', width: '25px'}} />
-                    <AiFillTwitterCircle style={{marginLeft: '10px', height: '25px', width: '25px'}}  />
-                    <TiSocialLinkedinCircular style={{marginLeft: '10px', height: '25px', width: '25px'}}  />
-                    <BsInstagram  style={{marginLeft: '10px', height: '25px', width: '25px'}} />
-                    <BsPinterest  style={{marginLeft: '10px', height: '25px', width: '25px'}} />
-                </div>
-                <div>
-                <hr></hr>
-                <p>© 2022 All Rights Reserved By Free Html Templates</p>
-               </div>
-            </div>
+            <Container fluid>
+                <Row>
+                    <Col>
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
+                            <h4>Reach at...</h4>
+                            <a className="hover-red"><GoLocation /><span>Location</span></a>
+                            <a className="hover-red"><IoCall /><span>+01-1234567890</span></a>
+                            <a className="hover-red"><AiOutlineMail /><span>demo@gmail.com</span></a>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+                            <Link to="/" className="link"><h4>Famms</h4></Link>
+                            <p>Necessary, making this the
+                                first true<br />generator on the Internet.
+                                It uses a<br />dictionary of over 200 Latin words, <br />
+                                combined with</p>
+                            <div className="container-row">
+                                <BsFacebook style={{ marginLeft: '10px', height: '25px', width: '25px' }} />
+                                <AiFillTwitterCircle style={{ marginLeft: '10px', height: '25px', width: '25px' }} />
+                                <TiSocialLinkedinCircular style={{ marginLeft: '10px', height: '25px', width: '25px' }} />
+                                <BsInstagram style={{ marginLeft: '10px', height: '25px', width: '25px' }} />
+                                <BsPinterest style={{ marginLeft: '10px', height: '25px', width: '25px' }} />
+                            </div>
+                            <div>
+                                <hr></hr>
+                                <p>© 2022 All Rights Reserved By Free Html Templates</p>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
