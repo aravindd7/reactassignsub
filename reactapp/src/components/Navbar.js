569d4c3a1f41b17af990caa655068877f2b3dcb2
aravindd7 <ak154761@gmail.com>
2022-01-28 10:05:17 +0530

    changes

--- reactapp/src/components/Navbar.js
+++ reactapp/src/components/Navbar.js
@@ -7,24 +7,27 @@ import { AiOutlineClose } from "react-icons/ai";
 import { DropdownButton, Dropdown } from "react-bootstrap";
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 
+
 export default function NavbarComp() {
     console.log("In Nav");
+    console.log('Navbar: ', Navbar);
+    console.log('Navbar: Toggle', Navbar.Toggle);
     return (
-        <Navbar bg="light" expand="lg">
-            <Container fluid>
+        <Navbar bg="light" expand="lg" className="nav-root">
+            <Container fluid className="container-nav">
                 <NavLink to="/"><Navbar.Brand><img src="./images/logo.png" width="250" /></Navbar.Brand></NavLink>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle aria-controls="basic-navbar-nav" style={{border: "none", color: "black"}}/>
                 <Navbar.Collapse>
-                    <Nav>
-                        <Nav.Link className="li-nav"><Link to="/"><span> Home</span> </Link></Nav.Link>
-                        <NavDropdown variant="light" className="dropdown-nav" title="Pages" >
-                            <Dropdown.Item as="button"><Link to="/about"><span>About</span></Link></Dropdown.Item>
-                            <Dropdown.Item as="button"><Link to="/testimonial"><span>Testimonial</span> </Link></Dropdown.Item>
+                    <Nav className="ul-nav">
+                        <Nav.Link className="li-nav"><Link to="/" className="link-nav"><span> HOME</span></Link></Nav.Link>
+                        <NavDropdown variant="light" className="dropdown-nav" title="Pages">
+                            <Dropdown.Item as="button" className="nav-dropitem"><Link to="/about" className="link-nav"><span>ABOUT</span></Link></Dropdown.Item>
+                            <Dropdown.Item as="button" className="nav-dropitem"><Link to="/testimonial" className="link-nav"><span>TESTIMONIAL</span> </Link></Dropdown.Item>
                         </NavDropdown>
-                        <Nav.Link className="li-nav"><Link to="/products"><span> Products </span> </Link></Nav.Link>
-                        <Nav.Link className="li-nav"><Link to="/blog"><span> Blog </span> </Link></Nav.Link>
-                        <Nav.Link className="li-nav"><Link to="/contact"><span> Contact </span> </Link></Nav.Link>
-                        <Nav.Link className="li-nav cart-icon"><Link to="/cart"> <BsCartFill />  </Link></Nav.Link>
+                        <Nav.Link className="li-nav"><Link to="/products" className="link-nav"><span> PRODUCTS </span> </Link></Nav.Link>
+                        <Nav.Link className="li-nav"><Link to="/blog" className="link-nav"><span> BLOG </span> </Link></Nav.Link>
+                        <Nav.Link className="li-nav"><Link to="/contact" className="link-nav"><span> CONTACT </span> </Link></Nav.Link>
+                        <Nav.Link className="li-nav cart-icon"><Link to="/cart" className="link-nav"> <BsCartFill />  </Link></Nav.Link>
                         <Nav.Link className="li-nav"><BsSearch /></Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
